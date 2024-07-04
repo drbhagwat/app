1559c70f95befd66e88db1a2ca1c50de17066eab
Dinesh Bhagwat <dinesh.bhagwat@gmail.com>
2024-07-04 11:32:09 +0530

    third commit - included bootstrap

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import styles from "./Header.module.css";
+import logo from "../../images/logo.png";
 
 const Header = () => {
   return (
@@ -8,29 +9,94 @@ const Header = () => {
           <h1>Welcome to the TestGenius</h1>
         </div>
       </header>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            <img src={logo} alt="Logo" className={styles.logo} />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Services
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Contact
+                </a>
+              </li>
 
-      <nav id={styles.navbar}>
-        <div className={styles.container}>
-          <ul>
-            <li>
-              <a href="#">Home</a>
-            </li>
-            <li>
-              <a href="#">About</a>
-            </li>
-            <li>
-              <a href="#">Services</a>
-            </li>
-            <li>
-              <a href="#">Contact</a>
-            </li>
-            <li>
-              <a href="#">Register (new)</a>
-            </li>
-            <li>
-              <a href="#">Login (member)</a>
-            </li>
-          </ul>
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Dropdown
+                </a>
+                <ul className="dropdown-menu">
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Are you New? - Register
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Subscribed to us? - Login
+                    </a>
+                  </li>
+                  <li>
+                    <hr className="dropdown-divider" />
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Something else here
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link disabled" aria-disabled="true">
+                  Disabled
+                </a>
+              </li>
+            </ul>
+            <form className="d-flex" role="search">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
         </div>
       </nav>
     </>
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -4,20 +4,23 @@ const Main = () => {
   return (
     <div className={styles.container}>
       <section id={styles.main}>
-        <h1>Welcome</h1>
+        <h1>Test Genius</h1>
         <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-          Necessitatibus beatae similique excepturi velit, consequatur magni
-          tenetur, aliquam, ipsa eveniet nam tempore minima repudiandae. Ipsa
-          odit maiores nihil tenetur nulla suscipit?
+          It tests the genius in you. We do have decades of experience as
+          engineers, computer scientists, and above all educators. This is our
+          dream product. We have put in a lot of effort in creating this. We
+          hope you will sign-up, subscribe, and take randomized online tests in
+          the areas of your interest. We sincerely hope you enjoy exploring
+          different facets of our product, as much as we did, in creating it.
         </p>
       </section>
 
       <aside id={styles.sidebar}>
         <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-          Necessitatibus beatae similique excepturi velit, consequatur magni
-          tenetur
+          We are open to you, our cusotmers! Let us know if we have not covered
+          your area of interest, or how we could interview. We promise we will
+          take a hard look at your feedback and include in our next version if
+          it is a win-win. Shoot an email to us at dinesh.bhagwat@gmail.com
         </p>
       </aside>
     </div>
